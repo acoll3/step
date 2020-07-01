fa966524cf9b20b661b802eaed4b31f1f43194ec
Andrea <acoll3@stanford.edu>
2020-07-01 18:17:55 +0000

    Created new servlet for the url '/delete-data' and implemented the POST route in new servlet

--- portfolio/src/main/java/com/google/sps/servlets/DeleteDataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteDataServlet.java
@@ -21,6 +21,7 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.Query.SortDirection;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -30,37 +31,26 @@ import javax.servlet.RequestDispatcher;
 import com.google.gson.*;
 
 /** Servlet that returns some example content. */
-@WebServlet("/data")
-public class DataServlet extends HttpServlet {
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
-    int numComments = Integer.parseInt(request.getParameter("number"));
-
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    PreparedQuery results = datastore.prepare(query);
-
-    ArrayList<String> comments = new ArrayList<String>();
-    Iterator<Entity> iter = results.asIterable().iterator();
-    for (int i = 0; i < numComments; i++) {
-        Entity e = iter.next();
-        String comment = (String) e.getProperty("text");
-        comments.add(comment);
-    }
-    response.setContentType("application/json;");
-    response.getWriter().println(convertToJsonUsingGson(comments));
-  }
+@WebServlet("/delete-data")
+public class DeleteDataServlet extends HttpServlet {
 
    @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String comment = request.getParameter("comment-input");
-    long timestamp = System.currentTimeMillis();
-
+    Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    Entity taskEntity = new Entity("Comment");
-    taskEntity.setProperty("text", comment);
-    taskEntity.setProperty("timestamp", timestamp);
-    datastore.put(taskEntity);
+    PreparedQuery results = datastore.prepare(query);
+
+    ArrayList<Key> keys = new ArrayList<Key>();
+    for (Entity e : results.asIterable()) {
+        keys.add(e.getKey());
+    }
+
+    // Iterator<Entity> iter = results.asIterable().iterator();
+    // for (int i = 0; i < results.asIterable().size(); i++) {
+    //     Entity e = iter.next();
+    //     Key key = e.getKey();
+        
+    // }
 
     response.sendRedirect("/index.html");
   }
